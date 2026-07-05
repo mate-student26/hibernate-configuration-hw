05a7aeba71aef46267e3c7f6b0f2646ba8798be8
Michal Brynkiewicz <michalbry@autograf.pl>
2026-07-05 22:56:38 +0200

    solution v1

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,19 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+import mate.academy.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        Movie movie = new Movie();
+        movie.setTitle("Titanic");
+        movie.setDescription("Description");
+        movieService.add(movie);
+
+        System.out.println(movieService.get(1L));
     }
 }
