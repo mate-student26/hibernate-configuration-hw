a7df18631c48ac222dfe980a694ddda1b72eee90
Michal Brynkiewicz <michalbry@autograf.pl>
2026-07-05 23:15:31 +0200

    solution v3

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -11,11 +11,6 @@ public class HibernateUtil {
     }
 
     private static SessionFactory initSessionFactory() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("Can't load JDBC driver", e);
-        }
         return new Configuration().configure().buildSessionFactory();
     }
 
